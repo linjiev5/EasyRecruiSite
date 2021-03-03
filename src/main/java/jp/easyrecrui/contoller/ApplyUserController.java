d913f129d6e949dec86d776fb08b4e8ecd4bbd65
linjiev5 <linjiev5@yahoo.co.jp>
2021-03-03 20:32:22 +0900

    mypage
    create myPage

--- src/main/java/jp/easyrecrui/contoller/ApplyUserController.java
+++ src/main/java/jp/easyrecrui/contoller/ApplyUserController.java
@@ -30,21 +30,23 @@ public class ApplyUserController {
 	@PostMapping(value = "/login")
 	public ModelAndView login(@RequestParam(name = "userId") String userId,
 			@RequestParam(name = "userPwd") String userPwd, Map<String, Object> message) {
+		ModelAndView mav = new ModelAndView("login/login");
 		User user = new User();
 		userPwd = Md5Code.md5Code(userPwd);
 		user.setUserId(userId);
 		user.setPassword(userPwd);
 		int result = applyUserService.findUser(user);
 		if (result == CodeType.USERNAME_NOT_EXIST.getCode()) {
-			ModelAndView mav = new ModelAndView("login/login");
+			// ユーザが存在しない場合
 			message.put("message", CodeType.USERNAME_NOT_EXIST.getMessage());
 			return mav;
 		} else if (result == CodeType.PASSWORD_ERROR.getCode()) {
-			ModelAndView mav = new ModelAndView("login/login");
+			//　パスワードが間違った場合
 			message.put("message", CodeType.PASSWORD_ERROR.getMessage());
 			return mav;
 		} else {
-			ModelAndView mav = new ModelAndView("main/main");
+			// ログイン成功
+			mav.setViewName("main/main");
 			return mav;
 		}
 
@@ -59,30 +61,67 @@ public class ApplyUserController {
 			@RequestParam(name = "mail") String mail, @RequestParam(name = "tel") String tel,
 			@RequestParam(name = "sex") int sex, @RequestParam(name = "adress") String adress,
 			@RequestParam(name = "birth") Date birth, Map<String, Object> message) {
-		User user = new User();
-		user.setUserId(userId);
-		user.setPassword(userPwd);
-		int result = applyUserService.findUser(user);
-		if(result == CodeType.USERNAME_NOT_EXIST.getCode()) {
-		ApplyUser applyUser = new ApplyUser();
-		userPwd = Md5Code.md5Code(userPwd);
-		applyUser.setAdress(adress);
-		applyUser.setMail(mail);
-		applyUser.setName(name);
-		applyUser.setPassword(userPwd);
-		applyUser.setSex(sex);
-		applyUser.setTel(tel);
-		applyUser.setUserId(userId);
-		applyUser.setBirth(birth);
-		applyUserService.addUser(applyUser);
-		message.put("message", CodeType.REGIST_SUCESS.getMessage());
-		ModelAndView mav = new ModelAndView("login/login");
-		return mav;
-		}else {
-			message.put("message", CodeType.USER_EXIST.getMessage());
-			ModelAndView mav = new ModelAndView("login/regist");
+		ModelAndView mav = new ModelAndView("login/regist");
+		// 空欄あるかどうかを確認
+		if ((sex == 1 || sex == 0)
+				&& !userId.toString().equals("") && !userPwd.toString().equals("") && !name.toString().equals("")
+				&& !mail.toString().equals("") && !tel.toString().equals("") && !adress.toString().equals("")
+				&& !birth.toString().equals("")) {
+			// 空欄がなければ登録行う
+			User user = new User();
+			user.setUserId(userId);
+			user.setPassword(userPwd);
+			int result = applyUserService.findUser(user);
+			// 判断した結果を処理する
+			if (result == CodeType.USERNAME_NOT_EXIST.getCode()) {
+				// ユーザが重複してなければ登録する
+				ApplyUser applyUser = new ApplyUser();
+				userPwd = Md5Code.md5Code(userPwd);
+				applyUser.setAdress(adress);
+				applyUser.setMail(mail);
+				applyUser.setName(name);
+				applyUser.setPassword(userPwd);
+				applyUser.setSex(sex);
+				applyUser.setTel(tel);
+				applyUser.setUserId(userId);
+				applyUser.setBirth(birth);
+				applyUserService.addUser(applyUser);
+				message.put("message", CodeType.REGIST_SUCESS.getMessage());
+				name = null;
+				userPwd = null;
+				adress = null;
+				mail = null;
+				tel = null;
+				userId = null;
+				birth = null;
+				mav.setViewName("login/login");
+				return mav;
+			} else {
+				// ユーザが存在すれば画面に提示する
+				message.put("message", CodeType.USER_EXIST.getMessage());
+				return mav;
+			}
+		} else {
+			// それぞれ未入力の項目の提示
+			if (userId.toString().trim().equals("")) {
+				message.put("message", "ユーザ名を入力してください");
+			} else if (userPwd.toString().trim().equals("")) {
+				message.put("message", "パスワードを入力してください");
+			} else if (name.toString().trim().equals("")) {
+				message.put("message", "名前を入力してください");
+			} else if (mail.toString().trim().equals("")) {
+				message.put("message", "メールを入力してください");
+			} else if (tel.toString().trim().equals("")) {
+				message.put("message", "電話番号を入力してください");
+			} else if (sex != 1 || sex != 0) {
+				message.put("message", "性別を選択してください");
+			} else if (adress.toString().trim().equals("")) {
+				message.put("message", "住所を入力してください");
+			} else if (birth.toString().trim().equals("")) {
+				message.put("message", "生年月日を選択してください");
+			} else {
+			}
 			return mav;
 		}
 	}
-
 }
--- src/main/java/jp/easyrecrui/contoller/IndexController.java
+++ src/main/java/jp/easyrecrui/contoller/IndexController.java
@@ -1,6 +1,7 @@
 package jp.easyrecrui.contoller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -9,38 +10,36 @@ import jp.easyrecrui.utils.UrlPath;
 @Controller
 public class IndexController {
 
-
 	@RequestMapping(UrlPath.MAIN_VIEW) // url地址值
 	public ModelAndView index() {
 		ModelAndView mav = new ModelAndView("main/main");// path,指向templates中的文件路径
 		return mav;
+
 	}
 
 	@RequestMapping(UrlPath.MYPAGE_VIEW)
-	public ModelAndView mypage() {
+	public ModelAndView mypage(Model model) {
 		ModelAndView mav = new ModelAndView("login/myPage");// テンプレートHTML指定
 		return mav;
 	}
 
 	@RequestMapping(UrlPath.JOBPAGE_VIEW)
-	public ModelAndView jobpage() {
-
+	public ModelAndView jobpage(Model model) {
 		ModelAndView mav = new ModelAndView("main/jobPage");// テンプレートHTML指定
+
 		return mav;
 	}
 
 	@RequestMapping(UrlPath.LOGIN)
-	public ModelAndView login() {
+	public ModelAndView login(Model model) {
 		ModelAndView mav = new ModelAndView("login/login");
 		return mav;
 	}
 
 	@RequestMapping(UrlPath.REGIST_VIEW)
-	public ModelAndView regist() {
-
+	public ModelAndView regist(Model model) {
 		ModelAndView mav = new ModelAndView("login/regist");
 		return mav;
 	}
-
 
 }
